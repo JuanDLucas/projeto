5c76fd762a0294969c66e6d793a77c3076d6b3d6
Juan D Lucas <juan.rodajau@gmail.com>
2026-05-11 20:20:48 -0300

    atualizações

--- src/index.js
+++ src/index.js
@@ -1,15 +1,68 @@
-// express trabalha com serviços http
 require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+// const operacoes = require('./lib/operacoes');
+const { somar, subtrair, multiplicar, 
+    dividir } = require('./lib/operacoes');
 
 app.get('/', (req, res) => {
-    res.send("Olá, Davi Boyzinho 76");
+    // res.send("Olá, mundo!!!");
+    res.json({ message: "Olá, mundo!" });
 });
 
 app.get('/welcome', (req, res) => {
-    const nome = req.query.nome || 'curioso';
-    res.send(`Olá, ${nome}`);
+    const name = req.query.nome || 'visitante';
+    // res.send(`Olá, ${name}`);
+    res.json({ message: `Olá, ${name}`});
+});
+
+app.get('/dobro/:numero', (req, res) => {
+    const numero = req.params.numero;
+
+    const dobro = numero * 2;
+
+    // res.send(`O dobro de ${numero} é ${dobro}.`);
+    res.json({ numero, dobro});
+});
+
+app.post('/operacoes/somar', (req, res) => {
+    const { numero1, numero2 } = req.body;
+
+    // const numero1 = req.body.numero1;
+    // const numero2 = req.body.numero2;
+
+    const resultado = somar(numero1, numero2);
+
+    res.json({ result: resultado });
+});
+
+app.post('/operacoes/subtrair', (req, res) => {
+    const { numero1, numero2 } = req.body;
+
+    const resultado = subtrair(numero1, numero2);
+
+    res.json({ result: resultado });
+});
+
+app.post('/operacoes/multiplicar', (req, res) => {
+    const { numero1, numero2 } = req.body;
+
+    const resultado = multiplicar(numero1, numero2);
+
+    res.json({ result: resultado });
+});
+
+app.post('/operacoes/dividir', (req, res) => {
+    const { numero1, numero2 } = req.body;
+
+    const resultado = dividir(numero1, numero2);
+
+    res.json({ result: resultado });
 });
 
 const PORTA = process.env.PORT || 3000;
